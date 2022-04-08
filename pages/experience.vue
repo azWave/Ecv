6cdb80fe42c320c210174431a3f28976713b7156
maximeboure <bourre.maxime.62@gmail.com>
2022-04-08 11:22:02 +0200

    delete old content add new content and update content's importing system

--- pages/experience.vue
+++ pages/experience.vue
@@ -1,23 +1,41 @@
 <template>
 <div>
-  <nuxt-content :document="page" />
+    <nuxt-content v-for="page in pages" :key="page.slug"
+    :document="page"/>
 </div>
   
 </template>
 
 <script>
+import{ mapGetters , mapActions} from 'vuex'
 export default {
-  name:"contact",
-  async asyncData ({ $content }) {
-    const page = await $content('experience').fetch()
-
-    return {
-      page
-    }
-  }
+  name:"expereinces",
+  async fetch() {
+  const pages = await this.$content("experiences").sortBy('slug', 'asc').fetch()
+  console.log('coucou')
+  this.setPages(pages)
+  },
+  methods: {
+    ...mapActions(['setPages']),
+  },
+  computed: {
+    ...mapGetters({ pages: 'getPages'}),
+  },
 }
 </script>
 
 <style>
+.flex{
+  display: flex;
+}
+.flex div{
+  width: 40%;
+  margin-left: 5%;
+  margin-right: 5%;
+}
 
+.flex h2{
+  text-decoration: underline 5px;
+  text-decoration-color: #73A1F8;
+}
 </style>
