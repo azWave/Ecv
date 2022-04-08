8620182ca4ae0a06f091635bcc740d19bf047859
maximeboure <bourre.maxime.62@gmail.com>
2022-04-08 13:20:21 +0200

    add projet's page and content

---
+++ content/projet.md
@@ -0,0 +1,15 @@
+---
+title: projets
+---
+
+<div class="content">
+
+# Mes projets
+
+<div class="cadres">
+
+<cadre content="<h2>Le pokedex</h2><p>un projet scolaire coder en vue.js</p>" src="pokemon.jpg" alt="pokedex"></cadre>
+
+<cadre content="<h2>Mon E-cv</h2><p>un E-cv fait via Nuxt2</p>" src="profil.jpg" alt="pokedex"></cadre>
+
+</div>
--- pages/projets.vue
+++ pages/projets.vue
@@ -1,6 +1,8 @@
 <template>
 <div>
-  Projet
+  <!-- <nuxt-content v-for="page in pages" :key="page.slug"
+    :document="page"/> -->
+  <nuxt-content :document="page" />
 </div>
   
   
@@ -8,6 +10,24 @@
 
 <script>
 export default {
+  name:"projets",
+  async asyncData ({ $content }) {
+    const page = await $content('projet').fetch()
+
+    return {
+      page
+    }
+  },
+  // async fetch() {
+  // const pages = await this.$content("accueil").sortBy('slug', 'asc').fetch()
+  // // this.setPages(pages)
+  // },
+  // methods: {
+  //   ...mapActions(['setPages']),
+  // },
+  // computed: {
+  //   ...mapGetters({ pages: 'getPages'}),
+  // },
 }
 </script>
 
